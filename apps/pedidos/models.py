7af62a9af104b49db2c9e664a21cb0a3840f0780
aclarinha1 <mlimadesiqueira2008@gmail.com>
2025-11-15 21:04:36 -0300

    criação de apps e criação das models

---
+++ apps/pedidos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from apps.produtos.models import Produto
+
+# Create your models here.
+
+class Pedido(models.Model):
+    data_hora = models.DateTimeField(auto_now_add=True)
+    valor_total = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    forma_pagamento = models.CharField(max_length=50)
+    e_encomenda = models.BooleanField(default=False)
+    #quem sabe colocar um atributo e_fiado, do tipo booleano, afinal, fiado não é tipo de pagamento
+    cliente = models.CharField(max_length=100, blank=True)
+    celular = models.CharField(max_length=15, blank=True)
+    data_hora_entrega = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Pedido #{self.id} - {self.data_hora.strftime('%d/%m/%Y %H:%M')}"
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='itens')
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.DecimalField(max_digits=3, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantidade} x {self.produto.nome_produto}"
---
+++ apps/produtos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nome_categoria = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    def __str__(self):
+        return self.nome_categoria
+    
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=100)
+    preco_unitario = models.DecimalField(max_digits=4, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='produtos')
+    def __str__(self):
+        return self.nome_produto
+
+
